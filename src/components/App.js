afc31c1aa8f08f5b6b8e113df146c02c37081a97
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-06-12 22:34:15 +0530

    updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,77 @@
-import React from 'react'
-import '../styles/App.css';
-
-
-const App = () => {
-  
+import React, { useState } from "react";
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    console.log(
+      'A form was submitted with Name :"' +
+        name +
+        '" and Email :"' +
+        email +
+        '"'
+    );
+    e.preventDefault();
+  };
   return (
-
-  )
+    <div className="App">
+      <header className="App-header">
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <h3> Sign-up Form </h3>
+          <label>Name:</label>
+          <br />
+          <input
+            type="text"
+            value={name}
+            id="name"
+            required
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+          <br />
+          <label>Email:</label>
+          <br />
+          <input
+            type="email"
+            value={email}
+            id="email"
+            required
+            onChange={(e) => {
+              handleEmailChange(e);
+            }}
+          />
+          <br />
+          <label>Password:</label>
+          <br />
+          <input
+            type="password"
+            value={password}
+            id="password"
+            required
+            onChange={(e) => {
+              handlePasswordChange(e);
+            }}
+          />
+          <br />
+          <br />
+          <button type="submit">Signup</button>
+        </form>
+      </header>
+    </div>
+  );
 }
-
-
 export default App;
